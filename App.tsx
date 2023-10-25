9f69a0ab7a6160cfced2759db9e1b91926348a07
Manuel Meneses <manuelmenesesg@gmail.com>
2023-10-25 02:44:00 +0100

    chore: add videos to scene

--- App.tsx
+++ App.tsx
@@ -1,31 +1,67 @@
 import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import {Alert, StyleSheet} from 'react-native';
 import {
   ViroARScene,
-  ViroText,
   ViroTrackingStateConstants,
   ViroARSceneNavigator,
+  ViroTrackingState,
+  ViroTrackingReason,
+  ViroVideo,
 } from '@viro-community/react-viro';
 
 const HelloWorldSceneAR = () => {
-  const [text, setText] = useState('Initializing AR...');
+  const [_, setText] = useState('Initializing AR...');
 
-  function onInitialized(state, reason) {
+  function onInitialized(state: ViroTrackingState, reason: ViroTrackingReason) {
     console.log('guncelleme', state, reason);
     if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
       setText('Hello World!');
-    } else if (state === ViroTrackingStateConstants.TRACKING_NONE) {
+    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
       // Handle loss of tracking
     }
   }
 
   return (
     <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
+      <ViroVideo
+        source={require('./src/assets/videos/number_1.mp4')}
+        height={2}
+        width={2}
+        loop={true}
+        position={[0, -3, -6]}
+        onClick={() => Alert.alert('Open video 1')}
+      />
+      <ViroVideo
+        source={require('./src/assets/videos/number_2.mp4')}
+        height={2}
+        width={2}
+        loop={true}
+        position={[0, 0, -6]}
+        onClick={() => Alert.alert('Open video 2')}
+      />
+      <ViroVideo
+        source={require('./src/assets/videos/number_3.mp4')}
+        height={2}
+        width={2}
+        loop={true}
+        position={[0, 3, -6]}
+        onClick={() => Alert.alert('Open video 3')}
+      />
+      <ViroVideo
+        source={require('./src/assets/videos/number_1.mp4')}
+        height={2}
+        width={2}
+        loop={true}
+        position={[-3, 0, -6]}
+        onClick={() => Alert.alert('Open video 4')}
+      />
+      <ViroVideo
+        source={require('./src/assets/videos/number_3.mp4')}
+        height={2}
+        width={2}
+        loop={true}
+        position={[3, 0, -6]}
+        onClick={() => Alert.alert('Open video 5')}
       />
     </ViroARScene>
   );
